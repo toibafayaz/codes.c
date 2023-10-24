8b3e67c5e0633c277a4393a7316628bbd6c42f32
toibafayaz <142816255+toibafayaz@users.noreply.github.com>
2023-10-24 11:36:52 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+         // Code for square of a number:
+         #include<stdio.h>
+         int main (){
+          int n;
+          printf("Enter a number :");
+          scanf("%d",&n);
+          int square =n*n;
+          printf("%d",square);
+          return 0;
+          }
+          
